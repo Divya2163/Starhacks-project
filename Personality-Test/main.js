02f498d675ca3c791b16341435fd335dda9f91d3
Divya Mohan <divyamohan210603@gmail.com>
2021-01-24 21:35:57 +0530

    Add files via upload

---
+++ Personality-Test/main.js
@@ -0,0 +1,110 @@
+//Set of questions and answers -DONE
+//Give each answer an identifier -DONE
+//Each identifier will increment through each question
+//St the end the identifier with the highest number is the winner 
+//Display the answer and rest the quiz
+
+//pass results frm previous question to the next page usig localcache
+
+//Randomise the background of the quiz for each questiion
+
+
+
+let currentQuestion = 0;
+let score = [];
+let selectedAnswersData = [];
+const totalQuestions =questions.length;
+
+const container = document.querySelector('.quiz-container');
+const questionEl = document.querySelector('.question');
+const option1 = document.querySelector('.option1');
+const option2 = document.querySelector('.option2');
+const option3 = document.querySelector('.option3');
+const nextButton = document.querySelector('.next');
+const previousButton = document.querySelector('.previous');
+const restartButton = document.querySelector('.restart');
+const result = document.querySelector('.result');
+
+//Function to generate question 
+function generateQuestions (index) {
+    //Select each question by passing it a particular index
+    const question = questions[index];
+    const option1Total = questions[index].answer1Total;
+    const option2Total = questions[index].answer2Total;
+    const option3Total = questions[index].answer3Total;
+    //Populate html elements 
+    questionEl.innerHTML = `${index + 1}. ${question.question}`
+    option1.setAttribute('data-total', `${option1Total}`);
+    option2.setAttribute('data-total', `${option2Total}`);
+    option3.setAttribute('data-total', `${option3Total}`);
+    option1.innerHTML = `${question.answer1}`
+    option2.innerHTML = `${question.answer2}`
+    option3.innerHTML = `${question.answer3}`
+}
+
+
+function loadNextQuestion () {
+    const selectedOption = document.querySelector('input[type="radio"]:checked');
+    //Check if there is a radio input checked
+    if(!selectedOption) {
+        alert('Please select your answer!');
+        return;
+    }
+    //Get value of selected radio
+    const answerScore = Number(selectedOption.nextElementSibling.getAttribute('data-total'));
+
+    ////Add the answer score to the score array
+    score.push(answerScore);
+
+    selectedAnswersData.push()
+    
+
+    const totalScore = score.reduce((total, currentNum) => total + currentNum);
+
+    //Finally we incement the current question number ( to be used as the index for each array)
+    currentQuestion++;
+
+        //once finished clear checked
+        selectedOption.checked = false;
+    //If quiz is on the final question
+    if(currentQuestion == totalQuestions - 1) {
+        nextButton.textContent = 'Finish';
+    }
+    //If the quiz is finished then we hide the questions container and show the results 
+    if(currentQuestion == totalQuestions) {
+        container.style.display = 'none';
+        result.innerHTML =
+         `<h1 class="final-score">Your score: ${totalScore}</h1>
+         <div class="summary">
+            <h1>Summary</h1>
+            <p>Possible - Your mental health condition, see below for a summary based on your results:</p>
+            <p>15 - 21- You are happy in your life lets make it even more happier</p>
+            <p>10 - 15 - You are missing only one piece to solve the riddle and win. You will win soon.</p>
+            <p>5 - 10 - You are just in half way to win</p>
+            <p>5 - No worries! You still have time. Starting now is better than never</p>
+        </div>
+        <button class="enter">StartGame</button>
+        `;
+    }
+    generateQuestions(currentQuestion);
+}
+
+//Function to load previous question
+function loadPreviousQuestion() {
+    //Decrement quentions index
+    currentQuestion--;
+    //remove last array value;
+    score.pop();
+    //Generate the question
+    generateQuestions(currentQuestion);
+}
+function startgame(){
+
+}
+
+generateQuestions(currentQuestion);
+nextButton.addEventListener('click', loadNextQuestion);
+previousButton.addEventListener('click',loadPreviousQuestion);
+result.addEventListener('click',startgame);
+
+
---
+++ Personality-Test/questions.js
@@ -0,0 +1,69 @@
+const questions = [
+    {
+      "question": "How do you think about yourself?",
+      "answer1": "Born to achieve",
+      "answer1Total": "3",
+      "answer2": "I am not enough lucky",
+      "answer2Total": "2",
+      "answer3": "I hate my self",
+      "answer3Total": "1"
+    },
+    {
+      "question": "I am happy with what I currently have.",
+      "answer1": "Agree",
+      "answer1Total": "3",
+      "answer2": "Neutral",
+      "answer2Total": "2",
+      "answer3": "Disagree",
+      "answer3Total": "1"
+    },
+    {
+      "question":
+        "Select in which order you would value these \"Money, Love & Career",
+      "answer1": "Love, Career, Money",
+      "answer1Total": "1",
+      "answer2": "Money, Career, Love",
+      "answer2Total": "3",
+      "answer3": "Career, Love, Money",
+      "answer3Total": "2"
+    },
+    {
+      "question": "Best Sentence to describe you?",
+      "answer1": "You feel superior to other people.",
+      "answer1Total": "3",
+      "answer2": "You consider yourself more practical than creative.",
+      "answer2Total": "2",
+      "answer3":
+        "Winning a debate matters less to you than making sure no one gets upset.",
+      "answer3Total": "1"
+    },
+    {
+      "question": "Which best describes your relationship with yourself",
+      "answer1": "You don't spend time with your self and don't know about self care.",
+      "answer1Total": "1",
+      "answer2": "You take care of yourself only when you are happy.",
+      "answer2Total": "2",
+      "answer3": "You always prioritize spending time with yourself and practice self care regularly .",
+      "answer3Total": "3"
+    },
+    {
+      "question":
+        "You make plans with a friend and they cancel on you, what do you do?",
+      "answer1":
+        "Say \"whatever\" and plan a night that'll be GREAT so they don't cancel again.",
+      "answer1Total": "3",
+      "answer2": "Feel hurt because you were looking forward to tonight.",
+      "answer2Total": "2",
+      "answer3": "No problem, you kinda wanted to stay home anyway.",
+      "answer3Total": "1"
+    },
+    {
+      "question": "Which is will be the most terrible suitation for you?",
+      "answer1": "Losing your friends",
+      "answer1Total": "1",
+      "answer2": "Losing your career",
+      "answer2Total": "2",
+      "answer3": "Losing your time",
+      "answer3Total": "3"
+    }
+]
